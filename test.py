87fd1dbc0266eceb7be30ad748ebf59d1d02d03b
Gary5691 <118814039+Gary5691@users.noreply.github.com>
2023-01-16 06:04:16 +0000

    working

--- test.py
+++ test.py
@@ -1,20 +1,30 @@
 import asyncio
+import random
 
-async def countdown_timer(n: int):
+
+list_items = []
+
+for i in range(1,100):
+    list_items.append(i)
+
+
+async def countdown_timer(n: int, item: str):
+    print(f'working on {item}')
     while n > 0:
-        print(f'{n} seconds remaining')
         await asyncio.sleep(1)
         n -= 1
-    print('Countdown complete!')
+    print(f"Done with {item}")
 
-# Start the timer in the background
-async def main():
-    task = asyncio.create_task(countdown_timer(5))
-    print('Countdown timer started')
-    await asyncio.sleep(1)
-    print('Doing some other work...')
-    await asyncio.sleep(3)
-    print('Finished other work')
-    await task
 
-asyncio.run(main())
+
+async def main(list_items: list):
+    semaphore = asyncio.Semaphore(3) # Number of tasks that can run in parallel
+    tasks = []
+    for item in list_items:
+        await semaphore.acquire()
+        task = asyncio.create_task(countdown_timer(random.randrange(1,100), item))
+        task.add_done_callback(lambda t: semaphore.release())
+        tasks.append(task)
+    await asyncio.gather(*tasks)
+
+asyncio.run(main(list_items))
